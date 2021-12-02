eccbc152aa155222137a940a55315233c9fc747a
Anis Draganovic <anis00723@gmail.com>
2021-12-03 00:26:21 +0100

    Initial commit

---
+++ nuit-info-api/src/main/java/com/nuitinfo/nuitinfoapi/NuitinfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.nuitinfo.nuitinfoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuitinfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuitinfoApiApplication.class, args);
+	}
+
+}
---
+++ nuit-info-api/src/test/java/com/nuitinfo/nuitinfoapi/NuitinfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuitinfo.nuitinfoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuitinfoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
